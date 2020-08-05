1fc85736e8931e73c2cd4a1225b54ce058a7f49f
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-08-05 10:49:30 +0300

    Added user profile

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import Routes from 'routes';
 import {BrowserRouter as Router} from 'react-router-dom';
 import TopBar from 'components/topbar/topbar';
 import {CurrentUserProvider} from 'contexts/currentUser';
+import CurrentUserChecker from 'components/currentUserChecker';
 
 const App = () => {
 return (
     <CurrentUserProvider>
+        <CurrentUserChecker>
         <Router>
             <TopBar/>
             <Routes/>
         </Router>
+        </CurrentUserChecker>
     </CurrentUserProvider>
 )
 };
