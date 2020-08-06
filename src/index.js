9f11725a42d8c7113bfabdac1ba9ccc2bfdf97a9
Kseniia Zinchenko <ukrksenia2@gmail.com>
2020-08-06 13:46:58 +0300

    The pagination implemented

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from 'routes';
 import {BrowserRouter as Router} from 'react-router-dom';
-import TopBar from 'components/topbar/topbar';
+import TopBar from 'components/topbar';
 import {CurrentUserProvider} from 'contexts/currentUser';
 import CurrentUserChecker from 'components/currentUserChecker';
 
